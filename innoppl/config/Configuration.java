6cb3d033c3c74d79a0f70e62c6e0f5702a25016e
surendarm <surendar.info@gmail.com>
2015-06-22 16:03:21 +0530

    Configuration file changed

--- innoppl/config/Configuration.java
+++ innoppl/config/Configuration.java
@@ -5,7 +5,8 @@ public class Configuration {
 	public static String SERVICE_URL = "https://avenger.mightyautoparts.com/CVIFTabletApp";
 	//public static String APPLICATION_URL = "https://avenger.mightyautoparts.com/ASSISTTabletApp";
 	public static String APPLICATION_URL = "http://localhost:8080/zdeppro";
-	
+	//comment added for git test;
+
 	public static String UPLOAD_PATH = "/home/innoppl/CVIFTabletApp/images/service_images/";
 	public static String Form_IMAGES_UPLOAD_PATH = "/home/innoppl/CVIFTabletApp/images/form_images/";
 	public static String SETTINGS_IMAGES_UPLOAD_PATH = "/home/innoppl/CVIFTabletApp/images/settings_images/";
